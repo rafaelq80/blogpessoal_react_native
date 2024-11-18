c6323e8c1b552cf6365fc8a5aac0a794486d9597
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2024-11-18 00:39:33 -0300

    :tada: Commit inicial

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+export default function Login() {
+	return (
+		<View className="flex-1 items-center justify-center">
+			<Text className="text-3xl font-bold text-indigo-900">
+				Projeto Blog Pessoal
+			</Text>
+		</View>
+	)
+}
